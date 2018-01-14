f7f8ca46e0d5aa486e7356523a9923a968bd6f8c
Bailey Cosier <bailey.cosier@gmail.com>
2018-01-15 02:55:41 +0700

    Enable auto colour detection

--- funcs.el
+++ funcs.el
@@ -37,7 +37,7 @@
   "Automatically load the appropiate colour scheme based
    on your horoscope, geophysical conditions, and day/night cycles."
   (interactive)
-  ;; (if (string= "light\n" (read-colour)) (baileys/light) (baileys/dark))
+  (if (string= "light\n" (read-colour)) (baileys/light) (baileys/dark))
   (message (concat "baileys: detected colour " (read-colour))))
 
 (defun baileys/clang-format-buffer-smart ()
@@ -62,5 +62,5 @@
 ;; Master initialisation functor
 
 (defun baileys/init ()
-  ; (baileys/auto-colour)
+  (baileys/auto-colour)
   (message "baileys: initialised"))
